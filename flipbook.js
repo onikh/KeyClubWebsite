69da6ca4c5c8ec7a67383cab5922cd8dca9016a7
onikh <onikh738@gmail.com>
2024-01-29 22:08:28 -0600

    flipbook center animation

--- flipbook.js
+++ flipbook.js
@@ -1,7 +1,29 @@
-$('#flipbook').turn({
-    width: 600,
-    height: 400,
-    page: 1,
-    autoCenter: true,
-    duration: 2000
-})
+
+var flipbook = $("#flipbook");
+
+$(window).resize(function () { /* do something */ });
+
+
+flipbook.turn({
+    width: 400,
+    height: 300,
+    autoCenter: true
+});
+
+
+
+
+
+jQuery.event.add(window, "resize", resizeFrame);
+
+function resizeFrame(){
+
+    var w = $('#main').width(); // this is div for ref
+
+    if(w<=320){
+    $('#flipbook').turn('size', '220', '287');
+    }else 
+    $('#flipbook').turn('size', '480', '626');
+    }
+
+
